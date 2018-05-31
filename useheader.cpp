0ac4919621ceaeace57fbf921d577175b8bd11c7
NathnaelDS <nathnaelds@gmail.com>
2018-06-01 01:30:50 +0300

    Add files via upload

---
+++ useheader.cpp
@@ -0,0 +1,13 @@
+#include"myheader"
+#include<iostream.h>
+
+
+int main(){
+    printSomeText();
+    return 0;
+}
+
+void printSomeText(){
+    cout<<"Defined a function declared in the header file";
+}
+
